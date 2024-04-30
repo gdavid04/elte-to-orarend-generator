d50350c808bdeaf06fef5a35a6e31fc6ea070916
GDavid <gabordavid04@gmail.com>
2024-04-30 13:51:03 +0200

    Fix #1, use unofficial query API

--- orarend.py
+++ orarend.py
@@ -5,12 +5,6 @@ from datetime import date
 import requests
 
 # TODO grafikus felület
-ev = date.today().year
-if date.today().month <= 6:
-	felev = f'{ev-1}-{ev}-2'
-else:
-	felev = f'{ev}-{ev+1}-1'
-print(f'Félév: {felev}')
 kurzusokRaw = pd.read_excel('export.xlsx')
 kurzusok = pd.DataFrame(kurzusokRaw, columns = [
 	'Tárgy neve', 'Kurzus típusa',
@@ -18,25 +12,32 @@ kurzusok = pd.DataFrame(kurzusokRaw, columns = [
 ])
 keresek = defaultdict(list)
 kurzusNevek = dict()
+kurzusTipusok = dict()
 for kurzus in kurzusok.itertuples():
 	keresek[kurzus[3]].append(kurzus[4])
-	kurzusNevek[kurzus[3] + ' ' + str(kurzus[4])] = f'{kurzus[1]}\n{kurzus[2]}'
-oraLista = pd.DataFrame()
+	kurzusNevek[kurzus[3] + ' ' + str(kurzus[4])] = kurzus[1]
+	kurzusTipusok[kurzus[3] + ' ' + str(kurzus[4])] = kurzus[2]
+oraLista = []
 for targykod, kurzuskodok in tqdm(keresek.items(), desc = 'Tanrend lekérdezése…'):
-	keres = requests.post(url = 'https://tanrend.elte.hu/oktatoitanrend.php', data = {
-		'mit': targykod,
-		'felev': felev,
-		'darab': '1000',
-		'submit': 'keres_kod_azon'
+	keres = requests.get(url = 'https://gabordavid.web.elte.hu/to/query', params = {
+		'subject_code': targykod
 	})
-	orakRaw = pd.read_html(keres.text, attrs = { 'id': 'resulttable' }, header = 0, flavor = 'bs4')
-	orak = pd.DataFrame(orakRaw[0], columns = [ 'Kurzuskod', 'Csop.', 'Idopont', 'Helyszin' ])
-	talalt = orak[orak['Csop.'].isin(kurzuskodok)]
+	if keres.json()['status'] != 'ok':
+		print(f'Hiba: {targykod}')
+		continue
+	orak = keres.json()['data']
 	for kurzuskod in kurzuskodok:
-		if not talalt['Csop.'].eq(kurzuskod).any():
-			nev = kurzusNevek[targykod + " " + str(kurzuskod)].replace('\n', ' ')
-			print(f'Nem található óra: {nev}')
-	oraLista = pd.concat([oraLista, talalt], ignore_index = True)
-oraLista.insert(0, 'Kurzus', (oraLista.pop('Kurzuskod') + ' ' + oraLista.pop('Csop.').astype('string')).map(kurzusNevek))
+		talalt = False
+		for ora in orak:
+			if ora['Csop.'] == str(kurzuskod):
+				oraLista.append(ora)
+				talalt = True
+		if not talalt:
+			oraLista.append({
+				'Kurzusnev': kurzusNevek[targykod + ' ' + str(kurzuskod)],
+				'Csop.': kurzuskod,
+				'Oratipus': kurzusTipusok[targykod + ' ' + str(kurzuskod)]
+			})
+			print(f'Nem található óra: {targykod} {kurzuskod}')
 
-oraLista.to_csv('orak.csv', index = False)
+pd.DataFrame(oraLista).to_csv('orak.csv', index = False)
--- tablazat.py
+++ tablazat.py
@@ -1,25 +1,34 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
+import math
 
-oraListaRaw = pd.read_csv('orak.csv')
-oraLista = pd.DataFrame(oraListaRaw, columns = [ 'Kurzus', 'Idopont', 'Helyszin' ])
+oraLista = pd.read_csv('orak.csv', dtype = str)
 
 fig, ax = plt.subplots()
-ax.axis([-0.5, 4.5, 24, 0])
 ax.set_xticks(range(5))
 ax.set_xticklabels([ 'Hétfő','Kedd', 'Szerda', 'Csütörtök', 'Péntek' ])
 ax.set_yticks(range(24))
 ax.set_yticklabels([ f'{ora}:00' for ora in range(24) ])
 ax.hlines(range(24), -0.5, 4.5, colors = 'lightgray', zorder = -1)
 ax.vlines([ n + 0.5 for n in range(5) ], 24, 0, colors = 'lightgray', zorder = -1)
-for ora in oraLista.itertuples():
-	m = re.match('(\\S+) ([0-9]+)\\:([0-9]+)\\-([0-9]+)\\:([0-9]+)', ora[2])
+minh, maxh = 24, 0
+for _, ora in oraLista.iterrows():
+	nev, csop = ora['Kurzusnev'], ora['Csop.']
+	if not isinstance(ora['Idopont'], str):
+		print(f'Hiányos: {nev} {csop}')
+		continue
+	m = re.match('(\\S+) ([0-9]+)\\:([0-9]+)\\-([0-9]+)\\:([0-9]+)', ora['Idopont'])
+	if not m:
+		print(f'Hiányos: {nev} {csop}')
+		continue
 	nap = { 'Hétfo': 0, 'Kedd': 1, 'Szerda': 2, 'Csütörtök': 3, 'Péntek': 4 }[m.group(1)]
 	oraKezd = int(m.group(2)) + int(m.group(3)) / 60
 	oraHossz = int(m.group(4)) + int(m.group(5)) / 60 - oraKezd
-	bar = ax.bar(nap, oraHossz, 1, oraKezd, edgecolor = 'black', color = 'lightblue' if ora[1].endswith('\nElőadás') else 'lightyellow')
-	ax.bar_label(bar, [ora[1] + '\n' + ora[3].removesuffix(" (Interaktív tábla)")], label_type = 'center')
+	bar = ax.bar(nap, oraHossz, 1, oraKezd, edgecolor = 'black', color = 'lightyellow' if ora['Oratipus'] == 'gyakorlat' else 'lightblue')
+	ax.bar_label(bar, [nev + '\n' + ora['Helyszin'].removesuffix(" (Interaktív tábla)")], label_type = 'center')
+	minh, maxh = min(minh, oraKezd), max(maxh, oraKezd + oraHossz)
+ax.axis([-0.5, 4.5, math.ceil(maxh), math.floor(minh)])
 
 fig.show()
 plt.waitforbuttonpress()
